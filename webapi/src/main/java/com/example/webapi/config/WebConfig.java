aee118fd52e07d5b0aaa0e6cabcba266570a1800
书剑江湖 <610725407@qq.com>
2023-12-11 21:46:28 +0800

    集成swagger

--- webapi/src/main/java/com/example/webapi/config/WebConfig.java
+++ webapi/src/main/java/com/example/webapi/config/WebConfig.java
@@ -19,33 +19,13 @@ public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptors).excludePathPatterns("/employees/login", "/doc.html", "/webjars/**")
-                .excludePathPatterns("/swagger-resources/**","/swagger-ui/**", "/v3/**", "/error");
-    }
-
-    /*@Bean
-    public Docket docket() {
-        ApiInfo apiInfo = new ApiInfoBuilder().title("员工管理").version("1.0").description("员工管理集成Swagger").build();
-        Docket docket = new Docket(DocumentationType.OAS_30).apiInfo(apiInfo).select().apis(RequestHandlerSelectors.basePackage("com.example.webapi.controller")).paths(PathSelectors.any()).build();
-        return docket;
-    }*/
-    @Bean
-    public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("十六进制说")
-                        .description("十六进制说API文档")
-                        .version("v1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("外部文档")
-                        .url("https://springshop.wiki.github.org/docs"));
+        registry.addInterceptor(loginInterceptors).excludePathPatterns("/employees/login", "/doc.html", "/webjars/**", "/favicon", "/v3/**");
     }
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
